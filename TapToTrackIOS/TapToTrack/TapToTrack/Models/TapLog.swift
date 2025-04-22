53aafcd50a1835d8a85dbe91ed92cb24373a0abb
tormodin <torm@kth.se>
2025-04-22 17:23:52 -0500

    added notes for logged presses

--- TapToTrackIOS/TapToTrack/TapToTrack/Models/TapLog.swift
+++ TapToTrackIOS/TapToTrack/TapToTrack/Models/TapLog.swift
@@ -10,11 +10,13 @@ struct TapLog: Identifiable, Codable {
     var id: UUID
     let timestamp: Date
     let type: String
+    var note: String?
 
-    init(id: UUID = UUID(), timestamp: Date, type: String) {
+    init(id: UUID = UUID(), timestamp: Date, type: String, note: String?=nil) {
         self.id = id
         self.timestamp = timestamp
         self.type = type
+        self.note = note
     }
 }
   
